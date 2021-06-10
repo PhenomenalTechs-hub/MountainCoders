58327980f96e6578bb732a105595b56198ffb9bf
Ali Ashraf <aliashraf@Devils-MacBook-Air.local>
2021-06-10 18:36:18 +0530

    initial commit

--- MountainCoders/blog/views.py
+++ MountainCoders/blog/views.py
@@ -16,7 +16,9 @@ def blog(request):
 
 
 def blogpost(request, slug):
-    return HttpResponse(f"You are Viewing {slug}")
+    blog = Blog.objects.filter(slug=slug).first()
+    context = {'blog': blog}
+    return render(request, 'blogpost.html', context)
 
 
 def search(request):
