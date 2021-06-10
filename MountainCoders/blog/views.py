d8b8b099b188e5d397db154f4babf2c47b9636e5
Ali Ashraf <aliashraf@Devils-MacBook-Air.local>
2021-06-10 12:45:12 +0530

    Creating Blog Model For our Mountain Coders Blog

---
+++ MountainCoders/blog/views.py
@@ -0,0 +1,27 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render
+from .models import Blog
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'index.html')
+
+
+def blog(request):
+    blogs = Blog.objects.all()
+    context = {'blogs': blogs}
+    return render(request, 'bloghome.html', context)
+
+
+def blogpost(request, slug):
+    return HttpResponse(f"You are Viewing {slug}")
+
+
+def search(request):
+    return render(request, 'search.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
